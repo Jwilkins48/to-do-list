9680de165b81269e9bbb69dd2da9c84c3d78c1af
Jenna <jenna48071@gmail.com>
2022-02-08 17:14:28 -0600

    finished design for project form/ added links

--- src/modules/addProject.js
+++ src/modules/addProject.js
@@ -1,20 +1,34 @@
-// const cancelBtn = document.querySelector('.cancelBtn');
 const projectForm = document.querySelector('.projectForm');
+const projectAddBtn = document.getElementById('projectSubmitBtn');
+const cancelBtn = document.querySelector('#projectCloseBtn');
 const addBtn = document.querySelector('.addProjectBtn');
+const projectNameInput = getElementById('projectName');
 
+// Opening and closing project form
 const addProject = () => {
     projectForm.classList.add('active');
 }
-
 const hideForm = () => {
+    projectForm.classList.remove('active');
+}
+
+const submitProject = () => {
 
 }
 
-const eventListeners = () =>{
-    addBtn.addEventListener('click', addProject);
-    // cancelBtn.addEventListener("click", hideForm);
 
-    return eventListeners;  
+
+
+
+const eventListeners = () => {
+    addBtn.addEventListener('click', addProject);
+    cancelBtn.addEventListener("click", hideForm);
+
+    projectAddBtn.addEventListener('click', submitProject);
+
+    return eventListeners;
 };
 
-export {eventListeners};
+export {
+    eventListeners
+};
